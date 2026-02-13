cbe7b58c22b27fe2f6eeb09ccdf2e827ebe48cd5
Chris Privitere <23177737+cprivitere@users.noreply.github.com>
2026-02-13 12:01:56 -0600

    updates

--- .oh-my-zsh-custom/aliases.zsh
+++ .oh-my-zsh-custom/aliases.zsh
@@ -1,11 +1,22 @@
+# ============================================================================
+# GENERAL ALIASES & CONFIGURATION
+# ============================================================================
+
+# Tool aliases
+alias tf=terraform
 alias kns=kubens
 alias kctx=kubectx
 alias cat='bat --paging=never -p'
+node-root() {
+  kubectl run node-root --restart=Never --rm -it --image=alpine --privileged \
+    --overrides '{"spec":{"hostPID":true}}' \
+    --override-type=merge \
+    --command -- nsenter --mount=/proc/1/ns/mnt -- /bin/bash
+}
+
+# Man page configuration
 export MANPAGER="env BATMAN_IS_BEING_MANPAGER=yes /bin/bash $HOMEBREW_PREFIX/bin/batman"
 export MANROFFOPT="-c"
 
+# Load kubectl aliases if available
 [ -f ~/.kubectl_aliases ] && source ~/.kubectl_aliases
-
-#Update brew and shell stuff
-alias upallthethings="brew update;brew outdated;brew upgrade;brew cleanup;fd -i -I --glob -H \*.zwc ~/.oh-my-zsh-custom -x rm;fd -d 2 -i -I --glob -H \*.zwc -x rm; cd ~/.oh-my-zsh-custom/plugins/fast-syntax-highlighting;git pull;cd ~/.oh-my-zsh-custom/plugins/zsh-autocomplete;git pull;cd ~/.oh-my-zsh-custom/plugins/zsh-autosuggestions;git pull;cd ~/.oh-my-zsh-custom/plugins/zsh-syntax-highlighting;git pull;cd ~/.oh-my-zsh-custom/themes/powerlevel10k;git pull;cd ~;omz update;omz reload"
-
